baa0698a3ce96d0b5817da46e3b6e6f790daa05c
vladimirvasylenko <vasylenkovladimir10@gmail.com>
2021-03-11 18:48:50 +0200

    lesson 3: homework1.0

--- src/com/hillel/lesson3/homework/Task1.java
+++ src/com/hillel/lesson3/homework/Task1.java
@@ -1,4 +1,19 @@
 package com.hillel.lesson3.homework;
 
+
+import java.util.Scanner;
+
 public class Task1 {
+
+    public static void main(String[] args){
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Введите Ваше имя");
+
+        if (myScanner.hasNextLine()){
+            String name = myScanner.nextLine();
+            System.out.println("Привет " + name);
+        }
+    }
+
 }
