06c77f71799ddb9eb2d509a847daea3483fe0725
vladimirvasylenko <vasylenkovladimir10@gmail.com>
2021-03-12 20:04:26 +0200

    lesson 3: homework1.1

--- src/com/hillel/lesson3/homework/Task1.java
+++ src/com/hillel/lesson3/homework/Task1.java
@@ -5,15 +5,54 @@ import java.util.Scanner;
 
 public class Task1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("Введите Ваше имя");
+        Scanner myName = new Scanner(System.in);
+        System.out.println("Введите Ваше имя:");
+        String name = myName.nextLine();
 
-        if (myScanner.hasNextLine()){
-            String name = myScanner.nextLine();
-            System.out.println("Привет " + name);
-        }
+
+        Scanner mySurmane = new Scanner(System.in);
+        System.out.println("Введите Вашу фамилию:");
+        String surname = mySurmane.nextLine();
+
+
+        Scanner myAge = new Scanner(System.in);
+        System.out.println("Укажите сколько Вам полных лет:");
+        int age = myAge.nextInt();
+
+
+//        if (myAge.hasNextInt()) {
+//            int age = myAge.nextInt();
+//        }
+//        else {
+//            System.out.println("Нужно ввести возраст!");
+//        }
+
+        Scanner myMail = new Scanner(System.in);
+        System.out.println("Введите Вашу почту для регистрации:");
+        String mail = myMail.nextLine();
+
+
+        Scanner myAbout = new Scanner(System.in);
+        System.out.println("Напишите несколько слов о себе:");
+        String about = myAbout.nextLine();
+
+        myName.close();
+        mySurmane.close();
+        myAge.close();
+        myMail.close();
+        myAbout.close();
+
+
+
+        System.out.println("Имя: " + name);
+        System.out.println("Фамилия: " + surname);
+        System.out.println("Возраст: " + age);
+        System.out.println("Почта: " + mail);
+        System.out.println("О себе: " + about);
+
+
     }
 
 }
---
+++ src/com/hillel/lesson3/homework/Task2.java
@@ -0,0 +1,23 @@
+package com.hillel.lesson3.homework;
+
+import java.util.Random;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        Random rnd = new Random();
+
+        int intrnd1 = rnd.nextInt();
+        int intrnd2 = rnd.nextInt();
+        int intrnd3 = rnd.nextInt();
+
+        double dblrnd1 = rnd.nextDouble();
+        double dblrnd2 = rnd.nextDouble();
+        double dblrnd3 = rnd.nextDouble();
+
+        double summall = intrnd1 + intrnd2 + intrnd3 + dblrnd1 + dblrnd2 + dblrnd3;
+
+
+        System.out.println(summall);
+    }
+}
