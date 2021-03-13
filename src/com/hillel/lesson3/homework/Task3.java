0f98d1fa371342bbfd5ddaa1bdbf5c77b680d4ab
vladimirvasylenko <vasylenkovladimir10@gmail.com>
2021-03-13 15:38:58 +0200

    lesson 3: homework1.2

---
+++ src/com/hillel/lesson3/homework/Task3.java
@@ -0,0 +1,19 @@
+package com.hillel.lesson3.homework;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        Scanner newScan = new Scanner(System.in);
+        System.out.println("Введите количество учасников :");
+        int scr = newScan.nextInt();
+
+        newScan.close();
+
+        Random newRand = new Random();
+        int finalRand = newRand.nextInt(scr)+1;
+        System.out.println("Поздравляем с победой участника под номером № " + finalRand);
+    }
+}
---
+++ src/com/hillel/lesson3/homework/Task4.java
@@ -0,0 +1,8 @@
+package com.hillel.lesson3.homework;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+
+    }
+}
