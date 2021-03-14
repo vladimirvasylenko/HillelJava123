b12b02a8de196d8692d9ccf80bb213a225ee725e
vladimirvasylenko <vasylenkovladimir10@gmail.com>
2021-03-14 19:32:21 +0200

    lesson 3: homework1.4

--- src/com/hillel/lesson3/homework/Task4.java
+++ src/com/hillel/lesson3/homework/Task4.java
@@ -4,12 +4,18 @@ public class Task4 {
 
     public static void main(String[] args) {
 
-        double sqrt = Math.sqrt(157);
+
         int abs = Math.abs(-57);
         int min = Math.min(57, 19);
         int max = Math.max(57, 19);
+        double sqrt = Math.sqrt(157);
+        double rand = Math.random();
+        double pow = Math.pow(32,12);
+        double round = Math.round(12.5);
+        double sin = Math.sin(57);
 
- //       System.out.println(sqrt);
+
+//        System.out.println(sin);
 
 
     }
---
+++ src/com/hillel/lesson3/homework/Task5.java
@@ -0,0 +1,9 @@
+package com.hillel.lesson3.homework;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
