9ccd5aad2f66f659c9e5b5b230ffdcc4f7c24c62
vladimirvasylenko <vasylenkovladimir10@gmail.com>
2021-04-08 17:00:44 +0300

    homework:9.3

--- src/com/hillel/lesson9/homework/Main.java
+++ src/com/hillel/lesson9/homework/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 //        User.textWriter();
-        System.out.println(Support.textChecker(Support.checkingString()));
+        System.out.println(Support.textChecker("hi"));
     }
 }
--- src/com/hillel/lesson9/homework/Support.java
+++ src/com/hillel/lesson9/homework/Support.java
@@ -1,6 +1,5 @@
 package com.hillel.lesson9.homework;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
@@ -11,31 +10,34 @@ public final class Support extends User {
         super(name, surname, mail, password, sex, country);
     }
 
-    String myString = checkingString();
+
 
      public static String checkingString(){
          Scanner myScanner = new Scanner(System.in);
          System.out.println("Введите строку для проверки : ");
-         return myScanner.nextLine();
+         String str = myScanner.nextLine();
+         myScanner.close();
+         return str;
      }
 
+     String test = Support.checkingString();
 
-    public static boolean textChecker(String myString) throws IOException {
+
+
+    public static boolean textChecker(String test) throws IOException {
 
 //        Scanner myScanner = new Scanner(System.in);
 //        System.out.println("Введите строку для проверки : ");
 //        String str = myScanner.nextLine();
 
-        boolean test = false;
 
         FileReader myFileReader = new FileReader(filePath);
         Scanner scannerCheck = new Scanner(myFileReader);
-        String checkStr = scannerCheck.nextLine();
-
 
         while (scannerCheck.hasNextLine()){
-            if (checkStr.equalsIgnoreCase(myString)) {
-                return test = true;
+            String checkStr = scannerCheck.nextLine();
+            if (checkStr.equalsIgnoreCase(checkingString())) {
+                return true;
             }
         }
 
@@ -43,7 +45,7 @@ public final class Support extends User {
         myFileReader.close();
         scannerCheck.close();
 
-        return test;
+        return false;
 
 
 
